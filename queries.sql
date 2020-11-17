48758f30696bda8acf2ded8400e644597060a1b3
Juliana Amoasei <juliana.amoasei@gmail.com>
2020-11-17 01:26:00 -0300

    add prisma client

--- queries.sql
+++ queries.sql
@@ -1,11 +1,24 @@
-CREATE TABLE user(
+-- sqlite
+
+CREATE TABLE User(
    id INTEGER PRIMARY KEY,
    nome TEXT NOT NULL,
    email TEXT NOT NULL,
    created_at TEXT DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE post(
+-- mysql
+
+CREATE TABLE User(
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   nome VARCHAR(255) NOT NULL,
+   email VARCHAR(255) NOT NULL,
+   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- sqlite
+
+CREATE TABLE Post(
 	id INTEGER PRIMARY KEY,
 	titulo TEXT NOT NULL,
    	conteudo TEXT NOT NULL,
@@ -15,17 +28,26 @@ CREATE TABLE post(
 	FOREIGN KEY(autorId) REFERENCES user(id)
 );
 
-INSERT INTO user (nome, email)
+-- mysql
+
+CREATE TABLE Post(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	titulo VARCHAR(255) NOT NULL,
+   conteudo TEXT NOT NULL,
+   autorId INT NOT NULL,
+   publicado BOOLEAN,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	FOREIGN KEY(autorId) REFERENCES User(id)
+);
+
+INSERT INTO User (nome, email)
 VALUES 
    ("Ana", "a@a.com"),
    ("Bia", "b@b.com"),
    ("Carla", "c@c.com");
 
-INSERT INTO post (titulo, conteudo, autorId, publicado)
+INSERT INTO Post (titulo, conteudo, autorId, publicado)
 VALUES 
    ("primeiro Ana", "bla bla",1 , true),
    ("primeiro Bia", "ble ble",2 , true),
    ("primeiro Carla", "bli bli",3 , false);
-   
-  SELECT * FROM user;
-  SELECT * FROM post;
