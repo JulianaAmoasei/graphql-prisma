cb9029807eaa8daef58759f646774a0223ce810e
Juliana Amoasei <juliana.amoasei@gmail.com>
2020-11-17 00:14:51 -0300

    prisma introspect tabelas user e post

--- queries.sql
+++ queries.sql
@@ -1,11 +1,11 @@
-CREATE TABLE User(
+CREATE TABLE user(
    id INTEGER PRIMARY KEY,
    nome TEXT NOT NULL,
    email TEXT NOT NULL,
    created_at TEXT DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE Post(
+CREATE TABLE post(
 	id INTEGER PRIMARY KEY,
 	titulo TEXT NOT NULL,
    	conteudo TEXT NOT NULL,
@@ -15,17 +15,17 @@ CREATE TABLE Post(
 	FOREIGN KEY(autorId) REFERENCES user(id)
 );
 
-INSERT INTO User (nome, email)
+INSERT INTO user (nome, email)
 VALUES 
    ("Ana", "a@a.com"),
    ("Bia", "b@b.com"),
    ("Carla", "c@c.com");
 
-INSERT INTO Post (titulo, conteudo, autorId, publicado)
+INSERT INTO post (titulo, conteudo, autorId, publicado)
 VALUES 
    ("primeiro Ana", "bla bla",1 , true),
    ("primeiro Bia", "ble ble",2 , true),
    ("primeiro Carla", "bli bli",3 , false);
    
-  SELECT * FROM User;
-  SELECT * FROM Post;
+  SELECT * FROM user;
+  SELECT * FROM post;
